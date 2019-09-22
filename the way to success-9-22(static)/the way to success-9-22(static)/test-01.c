671065223aa6da8266cb6b66ba50fb43e2a2b986
GuDwade <849467768@qq.com>
2019-09-22 14:58:43 +0800

    提交代码

---
+++ the way to success-9-22(static)/the way to success-9-22(static)/test-01.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//void test()
+//{
+//	static int i = 0;
+//	i++;
+//	printf("%d ", i);
+//}
+//
+//int main() 
+//{
+//	int i = 0;
+//	for(i=0; i<10; i++)
+//	{
+//		test();
+//	}
+//	return 0;
+//}
+extern int g_val;
+
+int main()
+{
+	printf("%d\n", g_val);
+
+	return 0;
+}
