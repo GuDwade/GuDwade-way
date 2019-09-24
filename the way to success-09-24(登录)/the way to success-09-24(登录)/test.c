77bd2985e7041d7bec81a1a1d5f143ae793bab67
GuDwade <849467768@qq.com>
2019-09-24 23:27:19 +0800

    提交代码

---
+++ the way to success-09-24(登录)/the way to success-09-24(登录)/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("ÇëÊäÈëÃÜÂë:>");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("µÇÂ¼³É¹¦\n");
+			break;
+		}
+		else;
+		{
+		printf("ÃÜÂë´íÎó\n");
+		}
+
+	}
+		if (i == 3)
+		{
+			printf("ÍË³ö³ÌÐò\n");
+		}
+
+	
+	return 0;
+}
